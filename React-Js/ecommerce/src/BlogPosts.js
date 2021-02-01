f5e90ec0cffc9bd572e68d3ca4e0eb1cfc0586a4
NAVEEN <navsoft18@gmail.com>
2021-02-01 19:59:15 +0530

    Add existing file

--- React-Js/ecommerce/src/BlogPosts.js
+++ React-Js/ecommerce/src/BlogPosts.js
@@ -5,23 +5,31 @@ export default(props) => {
     <section id="BlogPosts" class="py-5">
       <div className="container">
         <div className="row mt-5">
-          <div className="col-md-6">
-            <img src="https://source.unsplash.com/random/560x564/?travel" alt="" class="img-fluid"></img>
+          <div className="col-md-6 showcase-main text-white">
+            <h5 className="text-muted blog-title">Location 1</h5>
           </div>
           <div className="col-md-6 p-3">
             <div className="container">
               <div className="row">
                 <div className="col-md-6 mb-3">
                   <img src="https://source.unsplash.com/random/560x560/?travel" alt="" class="img-fluid"></img>
+                  <h5 className="text-muted blog-title">Location 1</h5>
+
                 </div>
-                <div className="col-md-6">
+                <div className="col-md-6 mb-3">
                   <img src="https://source.unsplash.com/random/560x561/?travel" alt="" class="img-fluid"></img>
+                  <h5 className="text-muted blog-title">Location 1</h5>
+
                 </div>
                 <div className="col-md-6">
                   <img src="https://source.unsplash.com/random/560x562/?travel" alt="" class="img-fluid"></img>
+                  <h5 className="text-muted blog-title">Location 1</h5>
+
                 </div>
                 <div className="col-md-6">
                   <img src="https://source.unsplash.com/random/560x563/?travel" alt="" class="img-fluid"></img>
+                  <h5 className="text-muted blog-title">Location 1</h5>
+
                 </div>
               </div>
             </div>
---
+++ Vue-Js/hello-world/src/HelloWorld.vue
@@ -0,0 +1,64 @@
+<template>
+  <h1>Hello World</h1>
+  <p>
+    I am Experimenting {{exp1}} and {{exp2}} {{1 === 1? 'Hello' :'Nop'}}
+  </p>
+  <h3 v-once>Item 1 :{{exp1}}</h3>
+  <h2 v-once>Item 2:{{exp2}}</h2>
+</template>
+
+<script>
+  export default {
+
+    data(){
+      return{
+        exp1:"React Js",
+        exp2:"Vue Js",
+      };
+    },
+
+    methods:{
+      makeAlert(){
+        alert("Sample Alert");
+      },
+      ChangeTopic(exp1,exp2,topic1,topic2){
+        exp1=topic1;
+        exp2=topic2;
+      },
+    },
+
+    mounted(){
+      console.log("I am mounted");
+      console.log(this.exp1);
+      // this.makeAlert();
+      const fm1 ='Angular';
+      const fm2 = 'jQuery';
+      this.ChangeTopic(this.exp1,this.exp2,fm1,fm2);
+      this.exp1 = 'Angular';
+      this.exp2 = 'jQuery';
+    },
+    beforeCreate(){
+      //first method called
+      console.log("beforCreate was called");
+      console.log(this.exp1);
+
+    },
+    created() {
+      console.log('created() was called');
+      console.log(this.exp1);
+
+    },
+    beforeMount(){
+      console.log('beforeMount() is called');
+      console.log(this.exp1);
+    },
+
+
+  }
+
+
+</script>
+
+<style scoped>
+
+</style>
