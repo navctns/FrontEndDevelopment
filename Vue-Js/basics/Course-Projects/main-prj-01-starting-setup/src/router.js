08b7815aa3216a6f186ec6ba0331f238742fe8d2
NAVEEN <navsoft18@gmail.com>
2021-05-10 21:14:43 +0530

    Add existing file

---
+++ Vue-Js/basics/Course-Projects/main-prj-01-starting-setup/src/router.js
@@ -0,0 +1,57 @@
+
+import { createRouter, createWebHistory } from 'vue-router';
+import CoachesList from './pages/coaches/CoachesList.vue';
+import CoachDetail from './pages/coaches/CoachDetail.vue';
+import CoachRegistration from './pages/coaches/CoachRegistration.vue';
+import ContactCoach from './pages/requests/ContactCoach.vue';
+import RequestsRecieved from './pages/requests/RequestsRecieved.vue';
+import NotFound from './pages/NotFound.vue';
+// import CoachContactForm from './pages/CoachContactForm.vue';
+
+const router = createRouter({
+  history:createWebHistory(),
+  routes:[
+    {
+      path:'/',
+      redirect:'/coaches'
+    },
+    {
+      path:'/coaches',
+      component: CoachesList,
+      name:'coaches',
+    },
+    {
+      path:'/coaches/:coachId',
+      props:true,
+      name:'coach-info',
+      component:CoachDetail,
+      children:[
+        {
+          path:'/coaches/:coachId/contact',
+          name:'contact',
+          component:ContactCoach,
+        },
+      ]
+    },
+    // {
+    //   path:'/contact',
+    //   name:'contact',
+    //   component:CoachContactForm,
+    // },
+    {
+      path:'/register',
+      component:CoachRegistration,
+    },
+    {
+      path:'/request',
+      component:RequestsRecieved,
+      name:'requests'
+    },
+    {
+      path:'/:notFound(.*)',
+      component:NotFound,
+    },
+  ]
+});
+
+export default router;
