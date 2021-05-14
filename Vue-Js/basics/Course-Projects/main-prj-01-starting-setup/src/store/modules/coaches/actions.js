366400001a28b0399f6d79836132e85d3220b4b4
NAVEEN <navsoft18@gmail.com>
2021-05-15 00:57:30 +0530

    Add existing file

--- Vue-Js/basics/Course-Projects/main-prj-01-starting-setup/src/store/modules/coaches/actions.js
+++ Vue-Js/basics/Course-Projects/main-prj-01-starting-setup/src/store/modules/coaches/actions.js
@@ -1,15 +1,29 @@
 export default{
-  registerCoach(context, data){
+  async registerCoach(context, data){
     console.log('action reg coach');
+    const userId = context.rootGetters.userId;
     const coachData = {
       // id:new Date().toISOString(),
-      id:context.rootGetters.userId,
+      // id:context.rootGetters.userId,
       firstName:data.first,
       lastName:data.last,
       description:data.desc,
       hourlyRate:data.rate,
       areas:data.areas,
     }
-    context.commit('registerCoach', coachData);
+    const response = await fetch(`https://coaches-app-2e5a0-default-rtdb.firebaseio.com/${userId}.json`, {
+      method:'PUT',
+      body:JSON.stringify(coachData),
+    });
+    // const responseData = await response.json();
+
+    if(!response.ok){
+      //Error handling;
+      console.log('Error handling');
+    }
+    context.commit('registerCoach', {
+      ...coachData,
+      id:userId,
+    });
   }
 }
--- Vue-Js/basics/Course-Projects/main-prj-01-starting-setup/src/store/modules/requests/actions.js
+++ Vue-Js/basics/Course-Projects/main-prj-01-starting-setup/src/store/modules/requests/actions.js
@@ -6,6 +6,7 @@ export default{
       message:payload.message,
       coachId:payload.coachId,
     };
+    // fetch(https://coaches-app-2e5a0-default-rtdb.firebaseio.com/')
     context.commit('addRequest', newRequest);
   }
 }
