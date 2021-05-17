cc9d457d06eca1d88de441425eae3e157d2cda80
NAVEEN <navsoft18@gmail.com>
2021-05-17 21:48:31 +0530

    Add existing file

---
+++ Vue-Js/basics/Course-Projects/main-prj-01-starting-setup/src/pages/NotFound.vue
@@ -0,0 +1,11 @@
+<template>
+  <section>
+    <base-card>
+      <h2>Page not found</h2>
+      <p>
+        This page could not be found may check out all our
+        <router-link to="/coaches">Coaches</router-link>
+      </p>
+    </base-card>
+  </section>
+</template>
